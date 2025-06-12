2cca648c59fc9bbfcd599ededb2cfca575b74570
zivivle <123868471+zivivle@users.noreply.github.com>
2025-06-12 12:50:53 +0900

    feat: 프로토타입 패턴

--- src/GrimpanHistory.ts
+++ src/GrimpanHistory.ts
@@ -1,9 +1,30 @@
 import { ChromeGrimpan, Grimpan, IEGrimpan } from "./Grimpan.js";
 
+interface Clonable {
+  clone(): Clonable;
+}
+
+class HistoryStack extends Array implements Clonable {
+  clone() {
+    return this.slice() as HistoryStack;
+  }
+}
+
 export abstract class GrimpanHistory {
   grimpan: Grimpan;
+  stack: HistoryStack;
+
   protected constructor(grimpan: Grimpan) {
     this.grimpan = grimpan;
+    this.stack = new HistoryStack();
+  }
+
+  getStack() {
+    return this.stack.clone();
+  }
+
+  setStack(stack: HistoryStack) {
+    this.stack = stack.clone();
   }
 
   abstract initialize(): void;
